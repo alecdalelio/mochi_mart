49976b8e3e7bc3ed93aeebadaf8da613cfca0229
alecdalelio <alecdalelio@gmail.com>
2021-03-25 12:14:58 -0400

    configured cors-headers and connected back end to front

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .products import products
 
 # Create your views here.
 
 
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('Hello', safe=False)
+    routes = [
+        'api/products/',
+        'api/products/create/',
+        'api/products/upload/',
+        'api/products/<id>/reviews/',
+        'api/products/top/',
+        'api/products/<id>/',
+        'api/products/delete/<id>/',
+        'api/products/<update>/<id>/',
+    ]
+
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    for prod in products:
+        if prod['_id'] == pk:
+            product = prod
+            break
+    return Response(product)
